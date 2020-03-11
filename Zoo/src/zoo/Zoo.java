74c722990eeeda1f64a1f938f6c17a0b7c0dc3f1
Williamads <39974819+wyllamcoelhoads@users.noreply.github.com>
2020-03-10 23:41:05 -0300

    Add files via upload
    demonstração de herança/agregação

---
+++ Zoo/src/zoo/Zoo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zoo;
+
+/**
+ *
+ * @author wylla
+ */
+public class Zoo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Cavalo trovao = new Cavalo();
+       Leao simba = new Leao();
+      simba.comer();
+    }
+    
+}
